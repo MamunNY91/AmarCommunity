8a3ffcfdcc8fe0a5f5a2096bed40d72da3baba1c
Abdullah A Mamun <abdullahamamun@Abdullahs-MacBook-Pro.local>
2017-08-22 18:31:56 -0400

    Module4

--- AmarCommunity/SignupVC.swift
+++ AmarCommunity/SignupVC.swift
@@ -7,26 +7,48 @@
 //
 
 import UIKit
+import Firebase
 
 
 
-class SignupVC: UIViewController {
+class SignupVC: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
     let plusButton:UIButton = {
         var bt = UIButton(type:.system)
         bt.setImage(UIImage(named:"plus_photo"), for: .normal)
-        
-        
+        bt.addTarget(self, action: #selector(handlePhoto), for: .touchUpInside)
         return bt
     }()
-         var  selectedDays : String?
+    func handlePhoto()  {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+           plusButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+           plusButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+        }
+        plusButton.layer.cornerRadius = plusButton.frame.width/2
+        plusButton.layer.masksToBounds = true
+        //
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+         //for saving user selection into selected
+         var  selectedCommunity : String?
+    
     // creating an array for uipickerview
     let communities = ["Bangladeshi","Indian","Chinese","Pakistani","Japanese"]
 
     
     let emailTextField:UITextField = {
         let tf = UITextField()
-        tf.placeholder = "Email"
+        tf.placeholder = "Enter valid Email"
         
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
         tf.borderStyle = .roundedRect
@@ -35,7 +57,7 @@ class SignupVC: UIViewController {
         return tf
     }()
     func handleTextInputchange()  {
-        let isFormValid = emailTextField.text?.characters.count ?? 0 > 0 && passwordTextField.text?.characters.count ?? 0 > 0 && userNameTextField.text?.characters.count ?? 0 > 0
+        let isFormValid = (emailTextField.text?.isValidEmail())! && passwordTextField.text?.characters.count ?? 0 >= 6 && userNameTextField.text?.characters.count ?? 0 > 0 && selectedCommunity?.characters.count ?? 0 > 0
         
         if isFormValid {
             signUpButton.isEnabled = true
@@ -58,7 +80,7 @@ class SignupVC: UIViewController {
     }()
     let passwordTextField:UITextField = {
         let tf = UITextField()
-        tf.placeholder = "Password"
+        tf.placeholder = "Enter password 6 characters long"
         tf.isSecureTextEntry = true
         
         tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
@@ -74,7 +96,7 @@ class SignupVC: UIViewController {
         bt.layer.cornerRadius = 5
         bt.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         bt.setTitleColor(.white, for: .normal)
-        //bt.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        bt.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
         bt.isEnabled = false
         return bt
         
@@ -97,6 +119,7 @@ class SignupVC: UIViewController {
         ct.backgroundColor = UIColor(white: 0, alpha: 0.03)
         ct.borderStyle = .roundedRect
         ct.font = UIFont.systemFont(ofSize: 14)
+        ct.addTarget(self, action: #selector(handleTextInputchange), for: .editingChanged)
         return ct
     }()
         
@@ -128,27 +151,51 @@ class SignupVC: UIViewController {
         communitySelector.inputView = picker
         createToolbar()
         setupInputFields()
-        
-        
     }
     
-    /*
+   
     
     func handleSignUp()  {
         guard let email = emailTextField.text, email.characters.count > 0
             else { return}
         guard let userName = userNameTextField.text,userName.characters.count>0 else { return}
         guard let password = passwordTextField.text,password.characters.count>0  else { return}
+        guard let community = communitySelector.text,community.characters.count>0  else { return}
         
         Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
             if let err = error {
                 print("Failed to create user",err)
             }
             print("Successfully created user" ,user?.uid ?? "")
+            //to save user's picture
+            //NS
+            let fileName = NSUUID().uuidString // create a unique string that identifies types,items etc.
+            guard let image = self.plusButton.imageView?.image else {return}
+            guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else {return}
+            Storage.storage().reference().child("Profile_images").child(fileName).putData(uploadData, metadata: nil, completion: { (metaData, err) in
+                if let err = err {
+                  print("Failed to upload profile image",err)
+                }
+                guard let profileImageUrl = metaData?.downloadURL()?.absoluteString else {return}
+                
+                print("Successfully uploaded profile image",profileImageUrl)
+                //to save user's info
+                guard let uid = user?.uid else{return}
+                let dictionaryValues = ["username":userName,"community":community,"ProfileImage":profileImageUrl]
+                let values = [uid:dictionaryValues]
+            
+                Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+                    if let err = err {
+                     print("Failed to create user info",err)
+                    }
+                    print("Successfully created user's info")
+                })
+            })
+            
         }
     }
     
-    */
+    
     
     fileprivate func setupInputFields()  {
         
@@ -161,6 +208,7 @@ class SignupVC: UIViewController {
         stackView.anchor(top: plusButton.bottomAnchor, paddingTop: 20, left: view.leftAnchor, paddingLeft: 40, right: view.rightAnchor, paddingRight: -40, bottom: nil, paddingBottom: 0, width: 0, height: 200)
         
     }
+    
     
 }
 
@@ -180,14 +228,20 @@ extension SignupVC: UIPickerViewDelegate,UIPickerViewDataSource {
         return communities[row]
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-           selectedDays = communities[row]
-           communitySelector.text = selectedDays
-         //self.view.endEditing(false)
+           selectedCommunity = communities[row]
+           communitySelector.text = selectedCommunity
+        
     }
 }
 
 
-
+extension String {
+    func isValidEmail() -> Bool {
+        // here, `try!` will always succeed because the pattern is valid
+        let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+        return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: characters.count)) != nil
+    }
+}
 
 
 
